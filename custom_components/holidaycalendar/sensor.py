fb3dcd56a92affa699d1627c623ca363c47cf95d
aalavender <jsdzh@hotmail.com>
2020-02-13 17:53:16 +0800

    修改利用sxtwl获取天干地支和农历

--- custom_components/holidaycalendar/sensor.py
+++ custom_components/holidaycalendar/sensor.py
@@ -1,6 +1,6 @@
 """
 A component which allows you to get holiday  and calendar infomation
-json接口的数据源：https://www.sojson.com/open/api/lunar/json.shtml
+使用https://pypi.org/project/sxtwl/, 这里的信息有，阴历，阳历，二十四节气，天干地支，星期几等
 For more details about this component, please refer to the documentation at
 https://github.com/aalavender/HolidayCalendar
 
@@ -9,17 +9,18 @@ import logging
 import asyncio
 import voluptuous as vol
 import datetime
+import time
 from homeassistant.helpers.entity import Entity
 import homeassistant.helpers.config_validation as cv
 from homeassistant.components.sensor import (PLATFORM_SCHEMA)
 from homeassistant.const import (CONF_NAME)
+import sxtwl
 import requests
-import json
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 _LOGGER = logging.getLogger(__name__)
 
-REQUIREMENTS = ["requests"]
+REQUIREMENTS = ["sxtwl", "requests"]
 
 COMPONENT_REPO = 'https://github.com/aalavender/HolidayCalendar'
 SCAN_INTERVAL = datetime.timedelta(hours=12)
@@ -29,54 +30,15 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_NAME): cv.string,
 })
 
-'''以下的数据每年需要调整'''
-HOLIDAY = {
-    datetime.date(year=2019, month=1, day=1):  "元旦放假",
+Gan = ["甲", "乙", "丙", "丁", "戊", "己", "庚", "辛", "壬", "癸"]
+Zhi = ["子", "丑", "寅", "卯", "辰", "巳", "午", "未", "申", "酉", "戌", "亥"]
+ShX = ["鼠", "牛", "虎", "兔", "龙", "蛇", "马", "羊", "猴", "鸡", "狗", "猪"]
+numCn = ["零", "一", "二", "三", "四", "五", "六", "七", "八", "九", "十"]
+jqmc = ["冬至", "小寒", "大寒", "立春", "雨水", "惊蛰", "春分", "清明", "谷雨", "立夏", "小满", "芒种", "夏至", "小暑", "大暑", "立秋", "处暑","白露", "秋分", "寒露", "霜降", "立冬", "小雪", "大雪"]
+ymc = ["十一", "十二", "正", "二", "三", "四", "五", "六", "七", "八", "九", "十" ]
+rmc = ["初一", "初二", "初三", "初四", "初五", "初六", "初七", "初八", "初九", "初十", "十一", "十二", "十三", "十四", "十五", "十六", "十七", "十八", "十九", "二十", "廿一", "廿二", "廿三", "廿四", "廿五", "廿六", "廿七", "廿八", "廿九", "三十", "卅一"]
+xingqi = ["星期一", "星期二","星期三","星期四", "星期五","星期六","星期日"]
 
-    datetime.date(year=2019, month=2, day=4): "春节放假",
-    datetime.date(year=2019, month=2, day=5): "春节放假",
-    datetime.date(year=2019, month=2, day=6): "春节放假",
-    datetime.date(year=2019, month=2, day=7): "春节放假",
-    datetime.date(year=2019, month=2, day=8): "春节放假",
-    datetime.date(year=2019, month=2, day=9): "春节放假",
-    datetime.date(year=2019, month=2, day=10): "春节放假",
-
-    datetime.date(year=2019, month=4, day=5):  "清明节放假",
-    datetime.date(year=2019, month=4, day=6):  "清明节放假",
-    datetime.date(year=2019, month=4, day=7):  "清明节放假",
-
-    datetime.date(year=2019, month=5, day=1):  "劳动节放假",
-    datetime.date(year=2019, month=5, day=2):  "劳动节放假",
-    datetime.date(year=2019, month=5, day=3):  "劳动节放假",
-    datetime.date(year=2019, month=5, day=4):  "劳动节放假",
-
-    datetime.date(year=2019, month=6, day=7): "端午节放假",
-    datetime.date(year=2019, month=6, day=8): "端午节放假",
-    datetime.date(year=2019, month=6, day=9): "端午节放假",
-
-    datetime.date(year=2019, month=9, day=13): "中秋节放假",
-    datetime.date(year=2019, month=9, day=14): "中秋节放假",
-    datetime.date(year=2019, month=9, day=15): "中秋节放假",
-
-    datetime.date(year=2019, month=10, day=1): "国庆节放假",
-    datetime.date(year=2019, month=10, day=2): "国庆节放假",
-    datetime.date(year=2019, month=10, day=3): "国庆节放假",
-    datetime.date(year=2019, month=10, day=4): "国庆节放假",
-    datetime.date(year=2019, month=10, day=5): "国庆节放假",
-    datetime.date(year=2019, month=10, day=6): "国庆节放假",
-    datetime.date(year=2019, month=10, day=7): "国庆节放假",
-}
-
-# HOLIDAY_BUBAN = {
-#     datetime.date(year=2019, month=2, day=2):  "春节补班",
-#     datetime.date(year=2019, month=2, day=3):  "春节补班",
-#
-#     datetime.date(year=2019, month=4, day=28): "劳动节补班",
-#     datetime.date(year=2019, month=5, day=5): "劳动节补班",
-#
-#     datetime.date(year=2019, month=9, day=29): "国庆节补班",
-#     datetime.date(year=2019, month=10, day=12): "国庆节补班",
-# }
 
 @asyncio.coroutine
 def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
@@ -89,6 +51,8 @@ class HolidayCalSensor(Entity):
     def __init__(self, name):
         self._name = name
         self._entries = {}
+
+        self.lunar = sxtwl.Lunar()  #实例化日历库
 
     def getAnniversary(self, day):
         ''' 返回纪念日，没有返回None '''
@@ -104,32 +68,27 @@ class HolidayCalSensor(Entity):
             return "None"
 
     def update(self):
-        _LOGGER.info("sensor HolidayCalendar update from https://www.sojson.com/open/api/lunar/json.shtml")
-        header = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.79 Safari/537.36'
-        }
-        json_text = requests.get("https://www.sojson.com/open/api/lunar/json.shtml", headers=header).content
-        json_data = json.loads(json_text)
-        if json_data["message"] == "success": #查询成功
-            self._entries["animal"] = json_data["data"]["animal"]
-            self._entries["year"] = json_data["data"]["year"]
-            self._entries["month"] = json_data["data"]["month"]
-            self._entries["day"] = json_data["data"]["day"]
-            self._entries["week"] = json_data["data"]["week"]
-            self._entries["cyclicalYear"] = json_data["data"]["cyclicalYear"]
-            self._entries["cyclicalMonth"] = json_data["data"]["cyclicalMonth"]
-            self._entries["cyclicalDay"] = json_data["data"]["cyclicalDay"]
-            self._entries["lunar"] = "{0}月{1}".format(json_data["data"]["cnmonth"], json_data["data"]["cnday"])
-            self._entries["festival"] = 'None'
-            if json_data["data"]["festivalList"]:
-                self._entries["festival"] = '/'.join(json_data["data"]["festivalList"])
-            self._entries["jieqi"] = 'None'
-            for key, value in json_data["data"]["jieqi"].items():
-                if json_data["data"]["day"] == int(key):
-                    self._entries["jieqi"] = value
+        year = self.lunar.getYearCal(datetime.datetime.now().year)
+        day = self.lunar.getDayByLunar(datetime.datetime.now().year,datetime.datetime.now().month,datetime.datetime.now().day)
+        self._entries["animal"] = ShX[year.ShX]
+        self._entries["year"] = datetime.datetime.now().year
+        self._entries["month"] = datetime.datetime.now().month
+        self._entries["day"] = datetime.datetime.now().day
+        self._entries["week"] = xingqi[datetime.datetime.now().weekday()]
+        self._entries["cyclicalYear"] = Gan[day.Lyear2.tg] + Zhi[day.Lyear2.dz]
+        self._entries["cyclicalMonth"] = Gan[day.Lmonth2.tg] + Zhi[day.Lmonth2.dz]
+        self._entries["cyclicalDay"] = Gan[day.Lday2.tg]+Zhi[day.Lday2.dz]
+        if day.Lleap:
+            self._entries["lunar"] = "润{0}月{1}".format(ymc[day.Lmc], rmc[day.Ldi])
+        else:
+            self._entries["lunar"] = "{0}月{1}".format(ymc[day.Lmc], rmc[day.Ldi])
+        #节日
+        self._entries["festival"] = 'None'
+        #节气
+        self._entries["jieqi"] = 'None'
 
-            # 获取周年纪念日，目前只有生日
-            self._entries["jnr"] = self.getAnniversary(json_data["data"]["month"]+json_data["data"]["day"])
+        # 获取周年纪念日，目前只有生日
+        self._entries["jnr"] = self.getAnniversary(datetime.datetime.now().month + datetime.datetime.now().day)
 
     @property
     def name(self):
@@ -137,17 +96,15 @@ class HolidayCalSensor(Entity):
 
     @property
     def state(self):
-        '''状态一共有四种：1，工作日  2，周末 3，法定节假日 4，补班'''
-        now_day = datetime.date.today()
-        if now_day in HOLIDAY.keys():
-            return HOLIDAY[now_day]
-#        elif now_day in HOLIDAY_BUBAN.keys():
-#            return HOLIDAY_BUBAN[now_day]
-        # 判断是否为周六和周日
-        elif now_day.weekday() > 4:  #周六5/周日6
-            return "周末"
-        else:
-            return "工作日"
+        #:param day: 日期， 格式为 '20160404'
+        #:return: bool
+        api = 'http://tool.bitefu.net/jiari/'
+        params = {'d': time.strftime("%Y%m%d", time.localtime()), 'apiserviceid': 1116}
+        rep = requests.get(api, params)
+        if rep.status_code != 200:
+            return '无法获取节日数据'
+        res = rep.text
+        return "休息日" if res != "0" else "工作日"
 
     @property
     def icon(self):
